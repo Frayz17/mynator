9e96c08d80363eb8e1af73b194ca3b1904270ced
Andrey <andrydmit@gmail.com>
2020-02-13 16:54:58 +0200

    creating POST route

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const fs = require('fs');
+
+app.use(express.json());
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours
+    }
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  console.log(req.body);
+  res.send('Done');
+});
+
+app.listen(port, () => {
+  console.log(`App running onm ${port}...`);
+});
